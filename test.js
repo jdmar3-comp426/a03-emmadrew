c62e76481e8a473942a6c6db7515437950185981
Emma Drew <emmadrew@live.unc.edu>
2021-12-01 00:18:09 -0500

    count array function added in mild1

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-import { sumToString, getIncreasingArray } from "./src/mild/mild_1.js";
+import { sumToString, getIncreasingArray, maxAndMin, countArray } from "./src/mild/mild_1.js";
 
 console.log(sumToString(3,7));
 console.log(getIncreasingArray(4,8));
+console.log(maxAndMin([1,4,6,7,4,11,4]));
+console.log(countArray([2,5,7,3,1,2,3,4,6]));
