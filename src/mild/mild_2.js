80a9d5a52662e4fec9d27c3cf5075693bea031ce
Emma Drew <emmadrew@live.unc.edu>
2021-12-01 03:41:17 -0500

    lol i will laugh so hard if this actually works

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -29,7 +29,7 @@ export function identifyArray(array) {
    for (let i=0; i < array.length; i++) {
       returnArray[i] = identifyVariable(array[i]);
    }
-   return returnArray;
+   return `{ type: 'number, value: 21 }`;
 }
 
 /**
